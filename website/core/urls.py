ea3f8955f8d4789839f9363a49d397fa4b3f1d61
VinEllaine <maghirangvin@gmail.com>
2026-07-07 15:44:02 +0800

    Create animated homepage and stage selection page

--- website/core/urls.py
+++ website/core/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("stages/", views.stages, name="stages"),
+    path("viewer/<int:stage_id>/", views.viewer, name="viewer"),
+    path("hologram/<int:stage_id>/", views.hologram, name="hologram"),
 ]
--- website/core/views.py
+++ website/core/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+def stages(request):
+    return render(request, "stages.html")
+
+def viewer(request, stage_id):
+    return render(request, "viewer.html", {"stage_id": stage_id})
+
+def hologram(request, stage_id):
+    return render(request, "hologram.html", {"stage_id": stage_id})
